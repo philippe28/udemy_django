ad8b08189fbd0a720a2b9252068e22946f566fd5
philippe28 <phadc@hotmail.com>
2016-04-06 02:00:44 -0300

    sessao 2 primeiro admin

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Courses
 
-# Register your models here.
+class CoursesAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'slug', 'start_date', 'create_at')
+    search_fields = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Courses, CoursesAdmin)
--- courses/models.py
+++ courses/models.py
@@ -1,16 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class CourseManager(models.Manager):
+
+    def search(self, query):
+        return self.get_queryset().filter(
+            models.Q(name__icontains=query) | \
+            models.Q(description__icontains=query)
+        )
+
+
 class Courses(models.Model):
 
-    nome = models.CharField('Nome', max_length=100)
+    name = models.CharField('Nome', max_length=100)
     slug = models.SlugField('Atalho')
     description = models.TextField('Descriçao', blank=True)
     start_date = models.DateField(
         'Date de inicio', null=True, blank=True
     )
     image = models.ImageField(
-        upload_to='courses/images', verbose_name='Imagem'
+        upload_to='courses/images', verbose_name='Imagem',
+        null=True, blank=True
     )
     create_at = models.DateTimeField('Criado em', auto_now_add=True)
     update_at = models.DateTimeField('Criado em', auto_now=True)
+
+    objects = CourseManager()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
+        ordering = ['name']
