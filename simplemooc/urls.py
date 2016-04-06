ad3b4a73825544b77396a8bef71dc699e2dca184
philippe28 <phadc@hotmail.com>
2016-04-06 16:56:32 -0300

    sessao 3 pagina courses

--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -20,4 +20,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('core.urls', namespace='core')),
+    url(r'^courses/', include('courses.urls', namespace='courses')),
 ]
